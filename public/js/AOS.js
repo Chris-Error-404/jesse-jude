41fe9fe041504ab08da474615ff4eb4d1141c608
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-27 23:00:14 +0100

    AOS removed for mobile

--- public/js/AOS.js
+++ public/js/AOS.js
@@ -1,6 +1,10 @@
   AOS.init({
+    mirror: true,
     once: true,
     offset: 120,
     duration: 400,
     easing: 'ease',
+    disable: function() {
+        return window.innerWidth < 1024;
+    }
   });
