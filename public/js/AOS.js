f82a2b582aefaa0a7cfb185f23fe30f3fa7467a9
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-08-29 14:53:12 +0100

    about text aos removal

--- public/js/AOS.js
+++ public/js/AOS.js
@@ -1,10 +1,29 @@
+  //MAIN AOS 
   AOS.init({
     mirror: true,
     once: true,
     offset: 120,
     duration: 400,
     easing: 'ease',
+    //disable all AOS for mobile screens
  /*    disable: function() {
         return window.innerWidth < 1024;
     } */
   });
+
+
+  //REMOVE ABOUT TEXT AOS
+  function removeAOS () {
+    const aboutText = document.getElementById('aboutText');
+
+    if(window.innerWidth < 1024) {
+      aboutText.setAttribute('data-aos','');
+    }
+    else{
+      aboutText.setAttribute('data-aos','fade-left');
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    removeAOS()
+  })
